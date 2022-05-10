c15adf97fc049a18ba9b7c9606372eeb025694e7
Rene Hexel <rhx@users.noreply.github.com>
2022-05-10 19:49:48 +1000

    Add swift-nio-ssl

--- Package.swift
+++ Package.swift
@@ -5,16 +5,21 @@ import PackageDescription
 
 let package = Package(
     name: "ssh-client",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .watchOS(.v6),
+        .tvOS(.v13),
+    ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-nio-ssh.git", branch: "main"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
             name: "ssh-client",
-            dependencies: []),
+            dependencies: [.product(name: "NIOSSH", package: "swift-nio-ssh")]),
         .testTarget(
             name: "ssh-clientTests",
             dependencies: ["ssh-client"]),
